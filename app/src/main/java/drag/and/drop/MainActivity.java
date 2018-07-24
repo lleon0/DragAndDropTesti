e4c498996531f0a13a1a6c319a35126ef95cbde3
Lauri Niku <t5nila01@students.oamk.fi>
2018-07-24 13:03:52 +0300

    Sipsi

--- app/src/main/java/drag/and/drop/MainActivity.java
+++ app/src/main/java/drag/and/drop/MainActivity.java
@@ -1,5 +1,6 @@
 package drag.and.drop;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -7,6 +8,7 @@ import android.content.ClipData;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.DragEvent;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
@@ -15,22 +17,91 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridLayout topLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        topLeft = findViewById(R.id.topleft);
+        //topLeft = findViewById(R.id.topleft);
         findViewById(R.id.myimage1).setOnTouchListener(new MyTouchListener());
         findViewById(R.id.myimage2).setOnTouchListener(new MyTouchListener());
         findViewById(R.id.myimage3).setOnTouchListener(new MyTouchListener());
         findViewById(R.id.myimage4).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.myimage5).setOnTouchListener(new MyTouchListener());
         findViewById(R.id.topleft).setOnDragListener(new MyDragListener());
         findViewById(R.id.topright).setOnDragListener(new MyDragListener());
         findViewById(R.id.bottomleft).setOnDragListener(new MyDragListener());
-        findViewById(R.id.bottomright).setOnDragListener(new MyDragListener());
+        //findViewById(R.id.bottomright).setOnDragListener(new MyDragListener());
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View myimage6 = inflater.inflate(R.layout.swimmer_databox_layout, null);
+        // Add the new row before the add field button.
+
+        myimage6.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        myimage6.getLayoutParams().width = (int) 120;
+        myimage6.getLayoutParams().height = (int) 120;
+        myimage6.requestLayout();
+
+        topLeft.addView(myimage6, topLeft.getChildCount() - 1);
+        myimage6.setOnTouchListener(new MyTouchListener());
+
+        /*
+        // Creating a new RelativeLayout
+        LinearLayout linearLayout = new LinearLayout(this);
+
+        // Defining the RelativeLayout layout parameters.
+        // In this case I want to fill its parent
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        linearLayout.setLayoutParams(params);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        topLeft.addView(linearLayout);
+
+        // Creating a new TextView
+        TextView tv = new TextView(this);
+        tv.setText("Test");
+
+        // Defining the layout parameters of the TextView
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        // Setting the parameters on the TextView
+        tv.setLayoutParams(lp);
+
+        // Adding the TextView to the RelativeLayout as a child
+        linearLayout.addView(tv);
+        */
+    }
+
+    public void NewDatabox(View view) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View myimage6 = inflater.inflate(R.layout.swimmer_databox_layout, null);
+        // Add the new row before the add field button.
+
+        myimage6.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        myimage6.getLayoutParams().width = (int) 120;
+        myimage6.getLayoutParams().height = (int) 120;
+        myimage6.requestLayout();
+
+        topLeft.addView(myimage6, topLeft.getChildCount() - 1);
+        myimage6.setOnTouchListener(new MyTouchListener());
     }
 
     private final class MyTouchListener implements OnTouchListener {
@@ -86,5 +157,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
-
